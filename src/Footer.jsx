6c7071ef1d1bee11cb8034506b69e7bbbb14f5aa
AshfakAhmath <ashfakahmath57@gmail.com>
2026-06-22 19:54:29 +0530

    feat: initialize React project with Vite, add basic components and styles
    - Created package.json with dependencies and scripts for Vite and ESLint
    - Added favicon.svg and icons.svg for application branding
    - Implemented main application structure with App, Header, MainContent, and Footer components
    - Added basic styling in App.css and index.css
    - Configured Vite for React application in vite.config.js
    - Included pngwing.com image asset for header

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <small>&copy; 2026 Ashfak development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import img from "./assets/pngwing.com.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={img} alt="React" width="80px" />
+    </header>
+  );
+}
+
+export default Header;
