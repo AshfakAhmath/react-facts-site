90ee321c088a7688099e20e7437e4eda95a28404
AshfakAhmath <ashfakahmath57@gmail.com>
2026-06-22 20:12:32 +0530

    feat: add navigation menu to header component

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,6 +4,13 @@ function Header() {
   return (
     <header>
       <img src={img} alt="React" width="80px" />
+      <nav>
+        <ul className="nav-list">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 }
